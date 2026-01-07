af12d74836882a22bb85c12b170c8d5657df6484
Khushbakht <kbirfan2007@gmail.com>
2026-01-07 23:12:55 +0500

    Implementing OOP

--- Encryp-Decryp.py
+++ Encryp-Decryp.py
@@ -30,3 +30,33 @@ for letters in cipher_text:
 
 time.sleep (1)
 print(f"Orignal Message   : {plain_text}")
+
+
+#Another way to write this code (By implementing OOP):
+import random
+import string
+
+chars = " " + string.digits + string.punctuation + string.ascii_letters
+chars = list (chars)
+key = chars.copy()
+random.shuffle(key)
+
+class Encryption:
+    def __init__(self, plain_text):
+        self.plain_text = plain_text
+        cipher_text = ""
+        for letter in plain_text:
+            index = chars.index (letter)
+            cipher_text += key [index]
+        print (f"Cipher text: {cipher_text}")
+encrypt = Encryption(input ("Plain text: "))
+
+class Decryption:
+    def __init__(self, cipher_text):
+        self.cipher_text = cipher_text
+        plain_text = ""
+        for letter in cipher_text:
+            index = key.index (letter)
+            plain_text += chars [index]
+        print (f"Plain text: {plain_text}")
+decrypt = Decryption(input ("Cipher text: "))
