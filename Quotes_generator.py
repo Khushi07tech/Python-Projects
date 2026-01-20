11a4837d189250dc724ed07314a08721bab083fd
Khushbakht <kbirfan2007@gmail.com>
2026-01-20 21:46:57 +0500

    Added new features and input validation

--- Quotes_generator.py
+++ Quotes_generator.py
@@ -1,4 +1,6 @@
 import requests
+import time
+
 base_url = "https://zenquotes.io/api"
 def get_quote():
     url = f"{base_url}/random"
@@ -22,9 +24,9 @@ def fetch_quotes (num_quotes):
 def print_quotes(quote):
         print(f"🔮Quote = {quote['q']}")
         print(f"✨Author = {quote['a']}")
-        print("🎀✨🎆⭐👑💕🎀✨🎆⭐👑")
+        print("🎀✨🎆⭐👑💕" * 5)
 
-def print_quote_list(quote_list):
+def print_quote_list(quote_list, num_quotes):
     for q in quote_list:
         for key, value in q.items():
             if key == "h":
@@ -33,25 +35,36 @@ def print_quote_list(quote_list):
                 print(f"{key} ~ {value}")
         print ()
 
+    if len(quote_list) != num_quotes:
+        print (f"You asked for {num_quotes} quotes but only {len(q)} were provided due to some problem.")
+
 def get_user_input ():
     while True:
         try:
-            return int(input("Enter the number of quotes: "))
+            num_quotes = int(input("Enter the number of quotes: "))
+            if num_quotes <= 0 or num_quotes > 100:
+                raise ValueError
+            return num_quotes
         except ValueError:
-            print ("Please enter a valid number")
+            print ("Please enter a valid number between 1-100")
 
 #=========== MAIN FLOW ===========
+def main ():
+    num_quotes = get_user_input()
 
-num_quotes = get_user_input()
 
-quotes = fetch_quotes(num_quotes)
-for quote in quotes:
-    print_quotes(quote)
+    quotes = fetch_quotes(num_quotes)
+    for quote in quotes:
+        print_quotes(quote)
+        time.sleep(2)
 
-see_quote_list = input("Want to see the quote's list(y/n): ").lower()
-if see_quote_list != "y" and see_quote_list != "n":
-    print ("Invalid Input")
-elif see_quote_list == "n":
-    print ("OKIEE!🎀")
-else:
-    print_quote_list(quotes)
+    see_quote_list = input("Want to see the quote's list(y/n): ").lower()
+    if see_quote_list != "y" and see_quote_list != "n":
+        print ("Invalid Input")
+    elif see_quote_list == "n":
+        print ("OKIEE!🎀")
+    else:
+        print_quote_list(quotes, num_quotes)
+
+if __name__ == '__main__':
+    main()
